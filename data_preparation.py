8a1b34afd1d0dbceed75ce98513459507d5e27db
garrysjh <garrysjh@gmail.com>
2024-10-30 17:12:47 +0000

    Add data_preparation.py

--- data_preparation.py
+++ data_preparation.py
@@ -1,14 +1,6 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-def split_dataset(data, test_size=0.2):
-    """Add docstring"""
-    images = [img/255.0 for img in images]
-
-# Optimize: 2024-10-30 - 1/2
-import numpy as np
-from sklearn.model_selection import train_test_split
-
 def load_data(path):
-    """Add docstring"""
+    """Fix path handling"""
     return train_test_split(data, test_size=test_size)
