15f441f97a0b2aa78622c0948013c4563ef76f98
garrysjh <garrysjh@gmail.com>
2024-11-01 15:56:47 +0000

    Update data_preparation.py

--- data_preparation.py
+++ data_preparation.py
@@ -1,14 +1,6 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-def load_data(path):
+def split_dataset(data, test_size=0.2):
     """Fix path handling"""
-    return train_test_split(data, test_size=test_size)
-
-# Update: 2024-10-31 - 3/3
-import numpy as np
-from sklearn.model_selection import train_test_split
-
-def augment_data(samples):
-    """Add docstring"""
     return [np.fliplr(img) for img in samples]
