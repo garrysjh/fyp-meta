57cbab697732794e51d0ac424fdff9e5dfa98a22
garrysjh <garrysjh@gmail.com>
2024-11-05 17:56:47 +0000

    Remove data_preparation.py

--- data_preparation.py
+++ data_preparation.py
@@ -1,14 +1,6 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-def split_dataset(data, test_size=0.2):
+def load_data(path):
     """Fix path handling"""
     return [np.fliplr(img) for img in samples]
-
-# Refactor: 2024-11-04 - 2/2
-import numpy as np
-from sklearn.model_selection import train_test_split
-
-def augment_data(samples):
-    """Update loading logic"""
-    return np.load(path)
