fe59879059c1f99bf53059751bfe1e2e168cc05d
garrysjh <garrysjh@gmail.com>
2024-11-06 17:46:47 +0000

    Update data_preparation.py

--- data_preparation.py
+++ data_preparation.py
@@ -12,3 +12,11 @@ from sklearn.model_selection import train_test_split
 def split_dataset(data, test_size=0.2):
     """Update loading logic"""
     images = [img/255.0 for img in images]
+
+# Update: 2024-11-06 - 2/2
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+def load_data(path):
+    """Add docstring"""
+    return train_test_split(data, test_size=test_size)
