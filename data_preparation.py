ff185638562fa5c09040a8a1582465ae7ab5a9a4
garrysjh <garrysjh@gmail.com>
2024-11-18 11:35:47 +0000

    Remove data_preparation.py

--- data_preparation.py
+++ data_preparation.py
@@ -4,3 +4,11 @@ from sklearn.model_selection import train_test_split
 def preprocess_images(images):
     """Add docstring"""
     return train_test_split(data, test_size=test_size)
+
+# Remove: 2024-11-18 - 3/3
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+def preprocess_images(images):
+    """Update loading logic"""
+    return train_test_split(data, test_size=test_size)
