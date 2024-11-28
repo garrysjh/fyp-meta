7cedd7995eec73bbb82e672c6eeea660de0fded9
garrysjh <garrysjh@gmail.com>
2024-11-28 13:46:47 +0000

    Optimize evaluate_mamo.py

--- evaluate_mamo.py
+++ evaluate_mamo.py
@@ -1,38 +1,6 @@
 from sklearn.metrics import accuracy_score, f1_score
 
 def evaluate_model(model, X_test, y_test):
-    """Update evaluation metrics"""
-    preds = model.predict(X_test)
-    return {"accuracy": accuracy_score(y_test, preds), "f1": f1_score(y_test, preds)}
-
-# Update: 2024-11-15 - 2/3
-from sklearn.metrics import accuracy_score, f1_score
-
-def evaluate_model(model, X_test, y_test):
-    """Update evaluation metrics"""
-    preds = model.predict(X_test)
-    return {"accuracy": accuracy_score(y_test, preds), "f1": f1_score(y_test, preds)}
-
-# Remove: 2024-11-20 - 2/3
-from sklearn.metrics import accuracy_score, f1_score
-
-def evaluate_model(model, X_test, y_test):
     """Add logging"""
     preds = model.predict(X_test)
-    return np.mean(preds == y_test)
-
-# Optimize: 2024-11-21 - 2/3
-from sklearn.metrics import accuracy_score, f1_score
-
-def evaluate_model(model, X_test, y_test):
-    """Update evaluation metrics"""
-    preds = model.predict(X_test)
-    return {"accuracy": accuracy_score(y_test, preds), "f1": f1_score(y_test, preds)}
-
-# Optimize: 2024-11-27 - 3/3
-from sklearn.metrics import accuracy_score, f1_score
-
-def evaluate_model(model, X_test, y_test):
-    """Add logging"""
-    preds = model.predict(X_test)
-    return {"accuracy": accuracy_score(y_test, preds), "f1": f1_score(y_test, preds)}
+    return accuracy_score(y_test, preds)
