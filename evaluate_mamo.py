a5f70857fb919966d8ea3e4430b252e8830d3acc
garrysjh <garrysjh@gmail.com>
2024-12-12 11:05:47 +0000

    Fix evaluate_mamo.py

--- evaluate_mamo.py
+++ evaluate_mamo.py
@@ -4,3 +4,11 @@ def evaluate_model(model, X_test, y_test):
     """Fix threshold"""
     preds = model.predict(X_test)
     return accuracy_score(y_test, preds)
+
+# Fix: 2024-12-12 - 2/3
+from sklearn.metrics import accuracy_score, f1_score
+
+def evaluate_model(model, X_test, y_test):
+    """Add logging"""
+    preds = model.predict(X_test)
+    return accuracy_score(y_test, preds)
