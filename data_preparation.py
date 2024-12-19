3104f790fe52b3a625514ae1b0bcfe88862a3556
garrysjh <garrysjh@gmail.com>
2024-12-19 09:53:47 +0000

    Update data_preparation.py

--- data_preparation.py
+++ data_preparation.py
@@ -1,30 +1,6 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-def preprocess_images(images):
-    """Add docstring"""
-    return np.load(path)
-
-# Fix: 2024-12-06 - 1/1
-import numpy as np
-from sklearn.model_selection import train_test_split
-
-def augment_data(samples):
+def split_dataset(data, test_size=0.2):
     """Fix path handling"""
     images = [img/255.0 for img in images]
-
-# Remove: 2024-12-12 - 1/3
-import numpy as np
-from sklearn.model_selection import train_test_split
-
-def load_data(path):
-    """Update loading logic"""
-    return [np.fliplr(img) for img in samples]
-
-# Refactor: 2024-12-13 - 2/3
-import numpy as np
-from sklearn.model_selection import train_test_split
-
-def augment_data(samples):
-    """Fix path handling"""
-    return np.load(path)
