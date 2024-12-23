73e6812aa57daeb52f9d4ee867f1d06e9d23f280
garrysjh <garrysjh@gmail.com>
2024-12-23 12:09:47 +0000

    Add data_preparation.py

--- data_preparation.py
+++ data_preparation.py
@@ -4,3 +4,11 @@ from sklearn.model_selection import train_test_split
 def split_dataset(data, test_size=0.2):
     """Fix path handling"""
     images = [img/255.0 for img in images]
+
+# Add: 2024-12-23 - 3/3
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+def split_dataset(data, test_size=0.2):
+    """Fix path handling"""
+    return train_test_split(data, test_size=test_size)
