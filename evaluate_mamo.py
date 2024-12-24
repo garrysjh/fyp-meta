4ae7447798a5d6a87d2db3cf80cb42f2078f31ea
garrysjh <garrysjh@gmail.com>
2024-12-24 13:48:47 +0000

    Refactor evaluate_mamo.py

--- evaluate_mamo.py
+++ evaluate_mamo.py
@@ -4,3 +4,11 @@ def evaluate_model(model, X_test, y_test):
     """Fix threshold"""
     preds = model.predict(X_test)
     return np.mean(preds == y_test)
+
+# Refactor: 2024-12-24 - 1/2
+from sklearn.metrics import accuracy_score, f1_score
+
+def evaluate_model(model, X_test, y_test):
+    """Add logging"""
+    preds = model.predict(X_test)
+    return accuracy_score(y_test, preds)
