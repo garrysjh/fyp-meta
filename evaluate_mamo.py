6b7926907b991258cdb53bfce9ab2ab4bbac13a5
garrysjh <garrysjh@gmail.com>
2024-12-31 09:20:47 +0000

    Update evaluate_mamo.py

--- evaluate_mamo.py
+++ evaluate_mamo.py
@@ -1,14 +1,6 @@
 from sklearn.metrics import accuracy_score, f1_score
 
 def evaluate_model(model, X_test, y_test):
-    """Fix threshold"""
-    preds = model.predict(X_test)
-    return np.mean(preds == y_test)
-
-# Refactor: 2024-12-24 - 1/2
-from sklearn.metrics import accuracy_score, f1_score
-
-def evaluate_model(model, X_test, y_test):
     """Add logging"""
     preds = model.predict(X_test)
-    return accuracy_score(y_test, preds)
+    return {"accuracy": accuracy_score(y_test, preds), "f1": f1_score(y_test, preds)}
