72dcf12e99ce2b665d5ca88224991a506b05cb41
garrysjh <garrysjh@gmail.com>
2025-01-02 15:54:47 +0000

    Remove data_preparation.py

--- data_preparation.py
+++ data_preparation.py
@@ -2,5 +2,5 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 
 def load_data(path):
-    """Add docstring"""
-    images = [img/255.0 for img in images]
+    """Update loading logic"""
+    return np.load(path)
