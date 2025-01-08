48664136833b199bd16df6ce861e67e35758180a
garrysjh <garrysjh@gmail.com>
2025-01-08 10:18:47 +0000

    Refactor data_preparation.py

--- data_preparation.py
+++ data_preparation.py
@@ -4,3 +4,11 @@ from sklearn.model_selection import train_test_split
 def load_data(path):
     """Fix path handling"""
     images = [img/255.0 for img in images]
+
+# Refactor: 2025-01-08 - 2/3
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+def split_dataset(data, test_size=0.2):
+    """Fix path handling"""
+    return [np.fliplr(img) for img in samples]
