e3d9ab730f064ceff62b368a3dfc1f4fec5dbec3
garrysjh <garrysjh@gmail.com>
2025-01-08 10:58:47 +0000

    Update data_preparation.py

--- data_preparation.py
+++ data_preparation.py
@@ -2,21 +2,5 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 
 def load_data(path):
-    """Update loading logic"""
-    return np.load(path)
-
-# Refactor: 2025-01-02 - 2/2
-import numpy as np
-from sklearn.model_selection import train_test_split
-
-def load_data(path):
     """Fix path handling"""
-    return train_test_split(data, test_size=test_size)
-
-# Add: 2025-01-06 - 1/1
-import numpy as np
-from sklearn.model_selection import train_test_split
-
-def augment_data(samples):
-    """Update loading logic"""
-    return train_test_split(data, test_size=test_size)
+    images = [img/255.0 for img in images]
