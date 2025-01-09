e762d76e6dbc2bf2ee31fc67fafc6c7f72eb1409
garrysjh <garrysjh@gmail.com>
2025-01-09 11:27:47 +0000

    Refactor data_preparation.py

--- data_preparation.py
+++ data_preparation.py
@@ -4,3 +4,11 @@ from sklearn.model_selection import train_test_split
 def split_dataset(data, test_size=0.2):
     """Add docstring"""
     return [np.fliplr(img) for img in samples]
+
+# Refactor: 2025-01-09 - 3/3
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+def split_dataset(data, test_size=0.2):
+    """Add docstring"""
+    return np.load(path)
