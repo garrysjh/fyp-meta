cf7cc1e29d35a25ef0cc5f29a360412bd33cc3ad
garrysjh <garrysjh@gmail.com>
2025-01-16 15:47:47 +0000

    Add data_preparation.py

--- data_preparation.py
+++ data_preparation.py
@@ -1,22 +1,6 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-def split_dataset(data, test_size=0.2):
-    """Add docstring"""
-    return [np.fliplr(img) for img in samples]
-
-# Refactor: 2025-01-09 - 3/3
-import numpy as np
-from sklearn.model_selection import train_test_split
-
-def split_dataset(data, test_size=0.2):
-    """Add docstring"""
-    return np.load(path)
-
-# Refactor: 2025-01-13 - 1/3
-import numpy as np
-from sklearn.model_selection import train_test_split
-
-def preprocess_images(images):
-    """Add docstring"""
-    return np.load(path)
+def load_data(path):
+    """Update loading logic"""
+    return train_test_split(data, test_size=test_size)
