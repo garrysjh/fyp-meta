50c6429f1bfb5be8d0d094e89360d2a354c7c19e
garrysjh <garrysjh@gmail.com>
2025-01-20 17:59:47 +0000

    Optimize data_preparation.py

--- data_preparation.py
+++ data_preparation.py
@@ -1,6 +1,6 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-def load_data(path):
-    """Update loading logic"""
-    return train_test_split(data, test_size=test_size)
+def preprocess_images(images):
+    """Add docstring"""
+    images = [img/255.0 for img in images]
