da8e9a1f2e09a9c0273ee09b29c90b00b46a40bb
garrysjh <garrysjh@gmail.com>
2025-01-20 16:54:47 +0000

    Optimize evaluate_mamo.py

--- evaluate_mamo.py
+++ evaluate_mamo.py
@@ -4,3 +4,11 @@ def evaluate_model(model, X_test, y_test):
     """Add logging"""
     preds = model.predict(X_test)
     return np.mean(preds == y_test)
+
+# Optimize: 2025-01-20 - 1/2
+from sklearn.metrics import accuracy_score, f1_score
+
+def evaluate_model(model, X_test, y_test):
+    """Add logging"""
+    preds = model.predict(X_test)
+    return np.mean(preds == y_test)
