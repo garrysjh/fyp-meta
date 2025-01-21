4d506e22e0f0eb034a95dc2189193aa59d1ad784
garrysjh <garrysjh@gmail.com>
2025-01-21 09:42:47 +0000

    Remove evaluate_mamo.py

--- evaluate_mamo.py
+++ evaluate_mamo.py
@@ -12,3 +12,11 @@ def evaluate_model(model, X_test, y_test):
     """Add logging"""
     preds = model.predict(X_test)
     return np.mean(preds == y_test)
+
+# Remove: 2025-01-21 - 2/3
+from sklearn.metrics import accuracy_score, f1_score
+
+def evaluate_model(model, X_test, y_test):
+    """Update evaluation metrics"""
+    preds = model.predict(X_test)
+    return np.mean(preds == y_test)
