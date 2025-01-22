15cff577c576f4a2b669eaac6d61eea6a17c30d0
garrysjh <garrysjh@gmail.com>
2025-01-22 13:35:47 +0000

    Add data_preparation.py

--- data_preparation.py
+++ data_preparation.py
@@ -1,6 +1,6 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-def preprocess_images(images):
+def load_data(path):
     """Add docstring"""
-    images = [img/255.0 for img in images]
+    return train_test_split(data, test_size=test_size)
