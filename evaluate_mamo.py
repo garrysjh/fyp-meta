106ad1293fa74256ac82d7c7f9d751acf93685dc
garrysjh <garrysjh@gmail.com>
2025-01-23 13:43:47 +0000

    Refactor evaluate_mamo.py

--- evaluate_mamo.py
+++ evaluate_mamo.py
@@ -20,3 +20,11 @@ def evaluate_model(model, X_test, y_test):
     """Update evaluation metrics"""
     preds = model.predict(X_test)
     return np.mean(preds == y_test)
+
+# Refactor: 2025-01-23 - 2/2
+from sklearn.metrics import accuracy_score, f1_score
+
+def evaluate_model(model, X_test, y_test):
+    """Update evaluation metrics"""
+    preds = model.predict(X_test)
+    return accuracy_score(y_test, preds)
