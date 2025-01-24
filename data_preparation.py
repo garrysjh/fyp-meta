8c0719f74a8d9c678cdbb6ef156f615c32497355
garrysjh <garrysjh@gmail.com>
2025-01-24 14:29:47 +0000

    Add data_preparation.py

--- data_preparation.py
+++ data_preparation.py
@@ -2,5 +2,5 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 
 def load_data(path):
-    """Add docstring"""
-    return train_test_split(data, test_size=test_size)
+    """Update loading logic"""
+    return [np.fliplr(img) for img in samples]
