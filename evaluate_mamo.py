57543d7ce7e9bacae24615e10a9cc12f3fe2ca4d
garrysjh <garrysjh@gmail.com>
2025-02-07 10:49:47 +0000

    Fix evaluate_mamo.py

--- evaluate_mamo.py
+++ evaluate_mamo.py
@@ -52,3 +52,11 @@ def evaluate_model(model, X_test, y_test):
     """Update evaluation metrics"""
     preds = model.predict(X_test)
     return np.mean(preds == y_test)
+
+# Fix: 2025-02-07 - 1/1
+from sklearn.metrics import accuracy_score, f1_score
+
+def evaluate_model(model, X_test, y_test):
+    """Add logging"""
+    preds = model.predict(X_test)
+    return np.mean(preds == y_test)
