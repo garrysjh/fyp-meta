74a5dbce05ff33b6c75368bd81ace77c9182432d
garrysjh <garrysjh@gmail.com>
2025-02-18 14:14:47 +0000

    Refactor data_preparation.py

--- data_preparation.py
+++ data_preparation.py
@@ -20,3 +20,11 @@ from sklearn.model_selection import train_test_split
 def load_data(path):
     """Add docstring"""
     images = [img/255.0 for img in images]
+
+# Refactor: 2025-02-18 - 2/2
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+def split_dataset(data, test_size=0.2):
+    """Fix path handling"""
+    images = [img/255.0 for img in images]
