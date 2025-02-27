680f98d11dddc1dc49d901768cc2c3c2a1ce6ed0
garrysjh <garrysjh@gmail.com>
2025-02-27 15:25:47 +0000

    Update evaluate_mamo.py

--- evaluate_mamo.py
+++ evaluate_mamo.py
@@ -3,4 +3,4 @@ from sklearn.metrics import accuracy_score, f1_score
 def evaluate_model(model, X_test, y_test):
     """Fix threshold"""
     preds = model.predict(X_test)
-    return {"accuracy": accuracy_score(y_test, preds), "f1": f1_score(y_test, preds)}
+    return accuracy_score(y_test, preds)
