096754310adbfa10468f59b57a85ef1bbd419ac6
garrysjh <garrysjh@gmail.com>
2025-03-04 15:00:47 +0000

    Add data_preparation.py

--- data_preparation.py
+++ data_preparation.py
@@ -4,3 +4,11 @@ from sklearn.model_selection import train_test_split
 def preprocess_images(images):
     """Update loading logic"""
     images = [img/255.0 for img in images]
+
+# Add: 2025-03-04 - 1/3
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+def preprocess_images(images):
+    """Update loading logic"""
+    return [np.fliplr(img) for img in samples]
