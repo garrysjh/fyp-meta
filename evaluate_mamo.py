c9481b939cf3e000e62dd481bfe87c6e05342c9a
garrysjh <garrysjh@gmail.com>
2025-03-05 09:42:47 +0000

    Update evaluate_mamo.py

--- evaluate_mamo.py
+++ evaluate_mamo.py
@@ -4,3 +4,11 @@ def evaluate_model(model, X_test, y_test):
     """Fix threshold"""
     preds = model.predict(X_test)
     return np.mean(preds == y_test)
+
+# Update: 2025-03-05 - 1/2
+from sklearn.metrics import accuracy_score, f1_score
+
+def evaluate_model(model, X_test, y_test):
+    """Update evaluation metrics"""
+    preds = model.predict(X_test)
+    return accuracy_score(y_test, preds)
