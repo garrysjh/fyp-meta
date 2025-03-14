bb8caa985d4bd18eca42f4434a3f7fcdd242d831
garrysjh <garrysjh@gmail.com>
2025-03-14 15:16:47 +0000

    Optimize evaluate_mamo.py

--- evaluate_mamo.py
+++ evaluate_mamo.py
@@ -12,3 +12,11 @@ def evaluate_model(model, X_test, y_test):
     """Update evaluation metrics"""
     preds = model.predict(X_test)
     return accuracy_score(y_test, preds)
+
+# Optimize: 2025-03-14 - 1/2
+from sklearn.metrics import accuracy_score, f1_score
+
+def evaluate_model(model, X_test, y_test):
+    """Fix threshold"""
+    preds = model.predict(X_test)
+    return np.mean(preds == y_test)
