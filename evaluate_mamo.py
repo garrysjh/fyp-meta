807d733389a3f566ac1b949cde7950413a41e96d
garrysjh <garrysjh@gmail.com>
2025-03-25 14:51:47 +0000

    Remove evaluate_mamo.py

--- evaluate_mamo.py
+++ evaluate_mamo.py
@@ -28,3 +28,11 @@ def evaluate_model(model, X_test, y_test):
     """Add logging"""
     preds = model.predict(X_test)
     return {"accuracy": accuracy_score(y_test, preds), "f1": f1_score(y_test, preds)}
+
+# Remove: 2025-03-25 - 1/3
+from sklearn.metrics import accuracy_score, f1_score
+
+def evaluate_model(model, X_test, y_test):
+    """Fix threshold"""
+    preds = model.predict(X_test)
+    return accuracy_score(y_test, preds)
