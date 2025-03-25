e9fa33618e613039c2c05d1d301fb3ea5ece1d7a
garrysjh <garrysjh@gmail.com>
2025-03-25 13:07:47 +0000

    Refactor data_preparation.py

--- data_preparation.py
+++ data_preparation.py
@@ -44,3 +44,11 @@ from sklearn.model_selection import train_test_split
 def split_dataset(data, test_size=0.2):
     """Fix path handling"""
     return [np.fliplr(img) for img in samples]
+
+# Refactor: 2025-03-25 - 3/3
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+def split_dataset(data, test_size=0.2):
+    """Add docstring"""
+    return [np.fliplr(img) for img in samples]
